379253281571d885f9e99df76ed81b349c8dcbdf
Medhat-Alathmna <medhatalathmna@gmail.com>
2024-03-17 21:20:20 +0200

    0

---
+++ src/api/medicine-type/controllers/medicine-type.ts
@@ -0,0 +1,7 @@
+/**
+ * medicine-type controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::medicine-type.medicine-type');
---
+++ src/api/medicine-type/routes/medicine-type.ts
@@ -0,0 +1,7 @@
+/**
+ * medicine-type router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::medicine-type.medicine-type');
