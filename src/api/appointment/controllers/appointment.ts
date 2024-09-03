11549065ffdfdf69e51e6bd1a5cae3f196b71d87
Medhat-Alathmna <medhatalathmna@gmail.com>
2024-09-03 22:24:44 +0300

    0

--- src/api/appointment/controllers/appointment.ts
+++ src/api/appointment/controllers/appointment.ts
@@ -29,81 +29,77 @@ module.exports = createCoreController('api::appointment.appointment', ({ strapi 
         }
     },
     async booking(ctx) {
-        const { firstName, middleName, lastName, phone, fromDate, toDate ,address,number,employee } = ctx.request.body;
+        const { firstName, middleName, lastName, phone, fromDate, toDate, address, number, employee } = ctx.request.body;
         const lastEntry = await strapi.db.query('api::appointment.appointment').findMany({
             orderBy: { createdAt: 'desc' },
             limit: 1,
-          });
-    
-          // Extract the ID of the last created entry
-          const lastCreatedId = lastEntry[0]?.number;
-          let parts = lastCreatedId.split('-');
-          let lastPart = parseInt(parts[parts.length - 1], 10) + 1;
-          let incrementedLastPart = lastPart.toString().padStart(2, '0');
-          parts[parts.length - 1] = incrementedLastPart;
-          let incrementedNumberString = parts.join('-');
+        });
 
-
-
-
-
-       
+        // Extract the ID of the last created entry
+        const lastCreatedId = lastEntry[0]?.number;
+        let parts = lastCreatedId.split('-');
+        let lastPart = parseInt(parts[parts.length - 1], 10) + 1;
+        let incrementedLastPart = lastPart.toString().padStart(2, '0');
+        parts[parts.length - 1] = incrementedLastPart;
+        let incrementedNumberString = parts.join('-');
         try {
             const createAppo = await strapi.entityService.create('api::appointment.appointment', {
-                data:{
+                data: {
 
-                    employee:[employee],
-                customer: {
-                    firstName,
-                    middleName,
-                    lastName
-                  }, phone, fromDate,address,
-                  number:incrementedNumberString,
-                   toDate,approved: false,deposit:0,appoBy:'Mobile User',
-                hide: false,}
+                    employee: [employee],
+                    customer: {
+                        firstName,
+                        middleName,
+                        lastName
+                    }, 
+                    phone, fromDate, address, createBy: 'Mobile User',
+                    number: incrementedNumberString,
+                    toDate, approved: false, deposit: 0, appoBy: 'Mobile User',
+                    hide: false,
+                }
             });
-            ctx.send({ message: 'Booking Created', createAppo,incrementedNumberString });
+            ctx.send({ message: 'Booking Created', createAppo, incrementedNumberString });
         } catch (error) {
             return ctx.throw(400, error);
 
 
         }
     },
-    async servicesMobile(ctx) {       
+    async servicesMobile(ctx) {
         try {
             const entities = await strapi.entityService.findMany('api::service.service');
             ctx.send(entities);
         } catch (error) {
-            ctx.send({ message: error ,error});
+            ctx.send({ message: error, error });
 
         }
     },
-    async usersMobile(ctx) {       
+    async usersMobile(ctx) {
         try {
-            const entities = await strapi.entityService.findMany('plugin::users-permissions.user',{
+            const entities = await strapi.entityService.findMany('plugin::users-permissions.user', {
                 populate: { someRelation: true },
                 filters: {
                     blocked: { $eq: false },
                     isToday: { $eq: true },
-                    hide:{$eq:false}
+                    hide: { $eq: false }
                 }
             });
             ctx.send(entities);
         } catch (error) {
-            ctx.send({ message: error ,error});
+            ctx.send({ message: error, error });
 
         }
     },
     async publishedPosts(ctx) {
         try {
-            
+
             const publishedPosts = await strapi.entityService.findMany('api::mobile-ad.mobile-ad', {
                 populate: 'ads',
                 filters: {
                     published: { $eq: true },
                 },
                 sort: { createdAt: 'desc' },
-                
+
             });
 
 
@@ -116,20 +112,20 @@ module.exports = createCoreController('api::appointment.appointment', ({ strapi 
     },
     async getLastCreatedId(ctx) {
         try {
-          // Query the database to get the last created entry sorted by 'createdAt'
-          const lastEntry = await strapi.db.query('api::your-collection.your-collection').findMany({
-            orderBy: { createdAt: 'desc' },
-            limit: 1,
-          });
-    
-          // Extract the ID of the last created entry
-          const lastCreatedId = lastEntry[0]?.id;
-    
-          // Return the ID in the response
-          ctx.send({ lastCreatedId });
+            // Query the database to get the last created entry sorted by 'createdAt'
+            const lastEntry = await strapi.db.query('api::your-collection.your-collection').findMany({
+                orderBy: { createdAt: 'desc' },
+                limit: 1,
+            });
+
+            // Extract the ID of the last created entry
+            const lastCreatedId = lastEntry[0]?.id;
+
+            // Return the ID in the response
+            ctx.send({ lastCreatedId });
         } catch (error) {
-          ctx.throw(500, 'Unable to fetch the last created ID');
+            ctx.throw(500, 'Unable to fetch the last created ID');
         }
-      },
+    },
 
 }));
