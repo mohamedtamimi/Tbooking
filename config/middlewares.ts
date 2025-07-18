fa20e2db31ea66faaa4535c659743599494714fa
Medhat-Alathmna <medhatalathmna@gmail.com>
2025-07-18 11:30:25 +0300

    Integrate Expo notifications and CORS config
    Added @surunnuage/strapi-plugin-expo-notifications to dependencies and configured the plugin in plugins.ts. Updated middlewares.ts to set a custom CORS origin and allowed methods. Extended the user schema to include an expoPushToken field for push notification support.

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -3,7 +3,16 @@ export default [
   //   name: "global::logger", // Register your logger middleware
   //   config: {},
   // },
-  "strapi::errors",
+  'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://mohamedtamimi-001-site12.ktempurl.com'], // ضف موقعك هنا
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: '*',
+      credentials: true
+    },
+  },
   "strapi::security",
   "strapi::cors",
   "strapi::poweredBy",
