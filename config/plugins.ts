b6639c4dba3bbe63807a6df4b5207df84e8e02c5
Medhat-Alathmna <medhatalathmna@gmail.com>
2025-08-07 08:56:15 +0300

    Remove Expo notifications plugin
    Deleted the '@surunnuage/strapi-plugin-expo-notifications' dependency from package.json and package-lock.json, and removed its configuration from config/plugins.ts. This cleans up unused notification functionality.

--- config/plugins.ts
+++ config/plugins.ts
@@ -12,12 +12,7 @@ module.exports = ({ env }) => ({
         },
       },
     },
-    'expo-notifications': {
-    enabled: true,
-    config: {
-      expoAppToken: 'mzBQHHKyTi0TPJoza_0H0R3E-2FdsWtCnTEONiIi', // leave empty if you’re using personal Expo push tokens
-    },
-  },
+   
     
     
   });
